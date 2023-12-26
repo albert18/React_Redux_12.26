2e5fd5678c846f7af2bcedb138edceec15acd339
albert18 <erertech@gmail.com>
2023-12-26 14:02:59 -0500

    added bootstrap link

--- Third-ReactProject/taskopedia/src/index.js
+++ Third-ReactProject/taskopedia/src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom/client';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 function MainHeader() {
-  return(<h1>REACT COURSE</h1>)
+  return(<h1 className="text-primary">REACT COURSE</h1>)
+}
+
+const subHeaderStyle = {
+  color: "red",
+  backgroundColor:"yellow",
 }
 
 function SubHeader() {
-  return(<p>This will be an exciting course.</p>)
+  return(<p style={subHeaderStyle}>This will be an exciting course.</p>)
 }
 
 function Header() {
@@ -20,16 +25,23 @@ function Header() {
 }
 
 function MainBody() {
-  return(<h1>Main body HERE</h1>)
+  return(
+  <div>
+    <h1>Main body HERE</h1>
+  </div>
+  )
 }
 
 function MainFooter() {
-  return(<h1>Happy Coding!</h1>)
+  return(
+    <p style={{color:"red", backgroundColor:"blue"}}>Happy Coding!</p>
+  )
 }
 
 root.render(
   <div>
     <Header />
+    <MainBody />
     <MainFooter />
   </div>
 );
