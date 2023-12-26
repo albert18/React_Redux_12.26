58d3c464f0ac872c9e9bd38c9f6ced6d4205d804
albert18 <erertech@gmail.com>
2023-12-26 14:39:06 -0500

    added image and few boostraps

--- Third-ReactProject/taskopedia/src/index.js
+++ Third-ReactProject/taskopedia/src/index.js
@@ -27,16 +27,39 @@ function Header() {
 
 function MainBody() {
   const whatWeWillLearn = "React JS";
-  
+  const lectureCount = 3;
   return(
   <div>
     <p>In this course, we we learn {whatWeWillLearn} by building TaskOPedia!</p>
+    Total Lecture - 3
+    <br />
+    <br />
     <ul>
       <li>TEST 1</li>
       <li>TEST 2</li>
       <li>TEST 3</li>
     </ul>
+    <div>
+      Enter Task: <input className='form-control' maxLength={20} placeholder='Enter Notes' readOnly={false}/>
+    </div>
+    <br />
   </div>
+  )
+}
+
+function Students() {
+  const fullName = "John Doe"
+  const programmingExp=10;
+  return(
+    <div className='container p-4'>
+      <div className='row'>Students Enrolled</div>
+      <div className='row border p-1'>
+        <div className='col-2'>
+          <img src={`https://ui-avatars.com/api/?name=${fullName}`}></img>
+        </div>
+        <div className='col-10'>Programming Experience {programmingExp} years</div>
+      </div>
+    </div>
   )
 }
 
@@ -47,9 +70,10 @@ function MainFooter() {
 }
 
 root.render(
-  <div className='m-5'>
+  <div className='container-fluid'>
     <Header />
     <MainBody />
+    <Students />
     <MainFooter />
   </div>
 );
