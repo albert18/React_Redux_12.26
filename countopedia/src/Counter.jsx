3f6a66c7789d2b5669009b069bb93b2f3774fda3
albert18 <erertech@gmail.com>
2023-12-27 18:08:30 -0500

    Update

--- countopedia/src/Counter.jsx
+++ countopedia/src/Counter.jsx
@@ -1,18 +1,27 @@
 import React from "react";
 
 export default class Counter extends React.Component{
+    constructor(props) {
+        super(props);
+        this.handleAttack = this.handleAttack.bind(this);
+        this.defendAttack = this.defendAttack.bind(this);
+        this.state = {
+            count: 0
+        }
+    }
+
     handleAttack() {
-        alert("Attack Clicked");
+        this.setState({count: this.state.count + 1});
     }
 
     defendAttack() {
-        alert("Defend Clicked");
+        this.setState({count: this.state.count - 1});
     }
 
     render() {
         return(
         <div className="row text-white">
-            <h1>Counter: </h1>
+            <h1>Counter: {this.state.count} </h1>
             <button onClick={this.handleAttack} style={{ width: "200px"}}>+1</button>
             <button onClick={this.defendAttack} style={{ width: "200px"}}>+2</button>
         </div>) 
