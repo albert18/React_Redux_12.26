ec153ff5e3261b3515aaf41cb18115d5913c8eea
albert18 <erertech@gmail.com>
2023-12-27 17:42:34 -0500

    Update

--- countopedia/src/Header.jsx
+++ countopedia/src/Header.jsx
@@ -2,9 +2,9 @@ import logo from './images/react.png'
 
 function Header() {
     return (
-        <div className="pt-3 pl-2">
+        <div className="py-3 pl-2" style={{borderBottom: "1px solid #777"}}>
             <img src={logo} alt="" style={{height:"35px", verticalAlign:"top"}} />
-            <span className="h2 pt-4 text-white-50">React Course - CountOpedia</span>
+            <span className="h2 pt-4 m-2 text-white-50">React Course - CountOpedia</span>
         </div>        
     )
 }
--- countopedia/src/index.js
+++ countopedia/src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Header from './Header';
+import Counter from './Counter';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Header></Header>
+  <div className='container-fluid'>
+    <Header></Header>
+    <Counter></Counter>
+  </div>
 );
 
