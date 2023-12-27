b44d655259180e33bea9f94d75662b0c38f14cef
albert18 <erertech@gmail.com>
2023-12-27 17:48:30 -0500

    added onclick button

--- countopedia/src/Counter.jsx
+++ countopedia/src/Counter.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 
 export default class Counter extends React.Component{
+    handleAttack() {
+        alert("Attack Clicked");
+    }
+
+    defendAttack() {
+        alert("Defend Clicked");
+    }
+
     render() {
         return(
         <div className="row text-white">
             <h1>Counter: </h1>
-            <button style={{ width: "200px"}}>+1</button>
-            <button style={{ width: "200px"}}>+2</button>
+            <button onClick={this.handleAttack} style={{ width: "200px"}}>+1</button>
+            <button onClick={this.defendAttack} style={{ width: "200px"}}>+2</button>
         </div>) 
     }
 }
