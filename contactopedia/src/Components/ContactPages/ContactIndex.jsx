8073dee6d817a5cd2c845f6e0e3cff5d9a49a033
albert18 <erertech@gmail.com>
2023-12-28 17:19:27 -0500

    update

--- contactopedia/src/Components/ContactPages/ContactIndex.jsx
+++ contactopedia/src/Components/ContactPages/ContactIndex.jsx
@@ -44,7 +44,6 @@ class ContactIndex extends React.Component {
                         </div>
                         <div className="row py-2">
                             <div className="col-8 offset-2 row">
-                                <AddContact />
                             </div>
                         </div>
                         <div className="row py-2">
