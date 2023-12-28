03cdbe8b19440a1ecbc028cf616ca0dfc7aa69ea
albert18 <erertech@gmail.com>
2023-12-28 14:48:17 -0500

    update remove contact

---
+++ contactopedia/src/Components/Layout/Footer.jsx
@@ -0,0 +1,14 @@
+const Footer = () => {
+    return (
+        <div 
+            style={{
+                color:"#ffff",
+                marginTop:"10px",
+                borderTop: "1px solid #555",
+                textAlign: "center",
+        }}> Happy Coding! @ContactOpedia
+        </div>
+    );
+}
+
+export default Footer;
--- contactopedia/src/index.js
+++ contactopedia/src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Header from './Header';
+import Header from './Components/Layout/Header';
+import Footer from './Components/Layout/Footer'
+// import RemoveAllContact from './Components/ContactPages/RemoveAllContact'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <div className='container-fluid'>
     <Header></Header>
+    {/* <RemoveAllContact></RemoveAllContact> */}
+    <Footer></Footer>
   </div>
 );
 
