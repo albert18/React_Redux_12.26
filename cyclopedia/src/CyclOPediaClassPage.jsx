f39f0920c81e205328e36eca0caad05f2189503b
albert18 <erertech@gmail.com>
2024-01-25 21:10:17 -0500

    update

--- cyclopedia/src/CyclOPediaClassPage.jsx
+++ cyclopedia/src/CyclOPediaClassPage.jsx
@@ -11,13 +11,30 @@ class CyclOpediaClassPage extends React.Component {
         }
     }
 
+    componentDidMount(){
+        console.log("Component did Mount");
+    }
+
+    componentDidUpdate(){
+        console.log("Component did Update");
+    }
+
+    componentWillUnmount(){
+        console.log("Component did Unmount");
+    }
+
     render() {
-        
+        console.log("Render Component");
+        return(
+            <div>
+                hello
+            </div>
+        ) 
 
     }
 }
 
-
+export default CyclOpediaClassPage;
 
 
 
