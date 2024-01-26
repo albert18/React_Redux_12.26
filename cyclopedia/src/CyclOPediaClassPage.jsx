3675ada8ea34f44c7e47d7ba55a440222cff0d11
albert18 <erertech@gmail.com>
2024-01-25 20:59:00 -0500

    added API user and install axios

---
+++ cyclopedia/src/CyclOPediaClassPage.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+class CyclOpediaClassPage extends React.Component {
+    constructor(props) {
+        super(props); 
+        this.state={
+            instructor: undefined,
+            studentList: [],
+            studentCount: 0,
+            hideInstructor: false
+        }
+    }
+
+    render() {
+        
+
+    }
+}
+
+
+
+
+
