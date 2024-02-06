dedd7b693a56ec757f407452d59791e31cdaf3d7
albert18 <erertech@gmail.com>
2024-02-05 20:50:00 -0500

    prevState

--- 5.watchopedia/src/Counter.jsx
+++ 5.watchopedia/src/Counter.jsx
@@ -5,24 +5,25 @@ const Counter = () => {
     const [counterState, setCounterState] = useState(() => {
         console.log("hello")
         //return 10;
-        return {counter: 10};
+        return {counter: 10, title: "Fun Counter"};
     });
 
     function incrementCounter() {
         setCounterState((prevState) => {
+            return { ...prevState, counter: prevState.counter + 1 };
             //return prevState + 1;
-            return { counter: prevState.counter + 1 };
+            // When using state un functional component, to make changes on particular value. You will need to add ...prevState and whatever value you want to pass.
         });
     };
 
     function decrementCounter() {
         setCounterState((prevState) => {
-            return { counter: prevState.counter - 1 };
+            return { ...prevState, counter: prevState.counter - 1 };
         });
     };
     return (
         <div className="col-12 col-md-2 offset-md-4 border text white">
-            <span className="h2 pt-4 m-2 text-white-50">Fun Counter</span>
+            <span className="h2 pt-4 m-2 text-white-50">{counterState.title}</span>
             <button onClick={incrementCounter} className="btn btn-success m-1">+1</button>
             <button onClick={decrementCounter} className="btn btn-danger m-1">-1</button>
             <br />
