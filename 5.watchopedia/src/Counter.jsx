aa17c0152409e2dfcc766633465605550a97e3f9
albert18 <erertech@gmail.com>
2024-02-05 20:39:59 -0500

    update

--- 5.watchopedia/src/Counter.jsx
+++ 5.watchopedia/src/Counter.jsx
@@ -1,16 +1,33 @@
 import { useState } from "react";
 
 const Counter = () => {
-    useState();
+
+    //const [counter, setCounter] = useState(10);  :: The different is when it execution, check console.log to see whats going on.
+    const [counter, setCounter] = useState(() => {
+        console.log("hello")
+        return 10;
+    });
+
+    function incrementCounter() {
+        setCounter((prevState) => {
+            return prevState + 1;
+        });
+    };
+
+    function decrementCounter() {
+        setCounter((prevState) => {
+            return prevState - 1;
+        });
+    };
     return (
         <div className="col-12 col-md-2 offset-md-4 border text white">
             <span className="h2 pt-4 m-2 text-white-50">Fun Counter</span>
-            <button className="btn btn-success m-1">+1</button>
-            <button className="btn btn-danger m-1">-1</button>
+            <button onClick={incrementCounter} className="btn btn-success m-1">+1</button>
+            <button onClick={decrementCounter} className="btn btn-danger m-1">-1</button>
             <br />
             <span className="h4">
                 Counter: &nbsp;
-                <span className="text-success">0</span>
+                <span className="text-success">{counter}</span>
             </span>
         </div>
     );
