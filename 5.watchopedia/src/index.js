517f95996771d87a5d9b5bb7224e5c81e638cf86
albert18 <erertech@gmail.com>
2024-02-05 20:12:29 -0500

    updated watchOpedia

--- 5.watchopedia/src/index.js
+++ 5.watchopedia/src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Header from './Header';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <div>
+    <Header />
+    <div className='row text-white'>
+      <div className='col-6'>
+        
+        
+      </div>
+    </div>
+  </div>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
