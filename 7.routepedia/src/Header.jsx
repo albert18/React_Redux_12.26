1820a0ee3727d0d62a7854545da3cb63715dc371
albert18 <erertech@gmail.com>
2024-02-07 19:22:15 -0500

    add pages

--- 7.routepedia/src/Header.jsx
+++ 7.routepedia/src/Header.jsx
@@ -2,9 +2,47 @@ import logo from './images/react.png'
 
 function Header() {
     return (
-        <div className="py-3 pl-2" style={{borderBottom: "1px solid #777"}}>
-            <img src={logo} alt="" style={{height:"35px", verticalAlign:"top"}} />
-            <span className="h2 pt-4 m-2 text-white-50">RoutePedia</span>
+        <div>
+            
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                <a className="navbar-brand" href="#"><img src={logo} className='ms-1' style={{height:"35px", verticalAlign:"top"}}></img> Routopedia</a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav mr-auto">
+                    <li className="nav-item active">
+                        <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Link</a>
+                    </li>
+                    <li className="nav-item dropdown">
+                        <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Dropdown
+                        </a>
+                        <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a className="dropdown-item" href="#">Action</a>
+                        <a className="dropdown-item" href="#">Another action</a>
+                        <div className="dropdown-divider"></div>
+                        <a className="dropdown-item" href="#">Something else here</a>
+                        </div>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link disabled" href="#">Disabled</a>
+                    </li>
+                    </ul>
+                    <form className="form-inline my-2 my-lg-0">
+                    {/* <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"> */}
+                    <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                    </form>
+                </div>
+                </nav>
+
+
+            {/* <img src={logo} alt="" style={{height:"35px", verticalAlign:"top"}} />
+            <span classNameName="h2 pt-4 m-2 text-white-50">RoutePedia</span> */}
         </div>        
     )
 }
