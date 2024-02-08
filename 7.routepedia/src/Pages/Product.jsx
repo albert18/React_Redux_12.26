53a644f238c3d256bbdec752d22d1ee93dcba686
albert18 <erertech@gmail.com>
2024-02-08 18:59:57 -0500

    using link to naviagete

--- 7.routepedia/src/Pages/Product.jsx
+++ 7.routepedia/src/Pages/Product.jsx
@@ -1,14 +1,22 @@
 import React from 'react'
-import  { useNavigate } from "react-router-dom";
+import  { Link, useNavigate } from "react-router-dom";
 
 function Product() {
   const navigate = useNavigate();
 
   return (
     <div>
-      <button className='form-control' onClick={() => {
-          navigate("/product/create")
-      }}>Add Product</button>
+      <button className='form-control' onClick={() => { navigate("/product/create")}}>Add Product</button>
+
+      <br />
+
+      <Link to={'/product/details/5'}>
+        <button className='form-control'>Navigate to Product Details - 5</button>
+      </Link>
+
+
+
+      
     </div>
   )
 }
