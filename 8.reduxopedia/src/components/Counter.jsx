4ccbbdcf6950bf5691f5f9c51533c5afd0a58534
albert18 <erertech@gmail.com>
2024-02-20 18:01:51 -0500

    update

--- 8.reduxopedia/src/components/Counter.jsx
+++ 8.reduxopedia/src/components/Counter.jsx
@@ -5,7 +5,22 @@ function Counter() {
 
   const count = useSelector((state) => state.counterStore.count);
   return (
-    <div>{count}</div>
+    <div className='mt-2 pt-3 pl-2 text-center'>
+      <div className='text-white pb-2 h4'>Counter: {count}</div>
+
+      <div className='row'>
+        <div className='p-4 col-12 col-md-6'>
+          <div className='border p-4'>
+            <button className='btn btn-primary'>Add</button> &nbsp;
+            <button className='btn btn-danger'>Remove</button>
+          </div>
+        </div>
+      </div>
+      
+
+
+
+    </div>
   )
 }
 
