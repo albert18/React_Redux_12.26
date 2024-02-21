84176e1097ab54c5f68603c99ea6b5a788a5873c
albert18 <erertech@gmail.com>
2024-02-20 20:59:07 -0500

    add destination slice

---
+++ 8.reduxopedia/src/redux/slice/destinationSlice.js
@@ -0,0 +1,30 @@
+import { createSlice } from "@reduxjs/toolkit"; 
+
+const initialState = () => {
+    return {
+        destinations: [
+            {
+                name: "Hong Kong",
+                days: 7,
+                fact: "World's longest covered escalator",
+            },
+            {
+                name: "Japan",
+                days: 10,
+                fact: "Japan is mostly mountains",
+            },
+            {
+                name: "New Zealand",
+                days: 14,
+                fact: "Last country in the world to be inhabited by humans",
+            },
+        ],
+    };
+};
+
+export const destinationsSlice = createSlice({
+    name: "destination",
+    initialState: initialState,
+    reducers: {}
+});
+export const destinationReducer = destinationsSlice.reducer;
--- 8.reduxopedia/src/redux/store.js
+++ 8.reduxopedia/src/redux/store.js
@@ -1,15 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { counterReducer } from "./slice/counterSlice";
+import { destinationReducer } from "./slice/destinationSlice"; 
 
 export const store = configureStore({
     reducer: {
         counterStore: counterReducer,
+        destinationStore: destinationReducer,
         // We will fill this reducer in the upcoming videos
     }
 });
 
-// debugger;
-//console.log(store.getState());
+debugger;
+console.log(store.getState());
 
 
 
